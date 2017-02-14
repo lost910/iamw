c74acac3cb89a4da644aad0ca12ffed471c6eefc
lost910 <sadirovnn@gmail.com>
2017-02-14 14:01:06 +0300

    Start

---
+++ iamw/ClientApp/app/components/sphelper/sphelper.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sp-helper',
+    template: require('./sphelper.component.html'),
+    styles: [require('./sphelper.component.css')]
+})
+export class SPHelperComponent {
+}
